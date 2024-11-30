02f43ef10eb05e0f0802726a3f04cb6c1b54cf9f
subhash prajapati <subhashme0094@gmail.com>
2024-11-30 12:39:06 +0530

    qtify

--- src/component/SearchBar/SearchBar.js
+++ src/component/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 // The imported PNG file is treated as a React component, and you can use it just like any other React component in your application.
-import SearchIcon from "../../assets/search-icon.png";
+import {ReactComponent as SearchIcon} from  "../../assets/search-icon.svg"
 import styles from "./SearchBar.module.css";
 
 const SearchBar = ({search}) => {
