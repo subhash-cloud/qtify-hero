c907ce288c06bca74f3bfb96cd4b6ac39afc2e8d
subhash prajapati <subhashme0094@gmail.com>
2024-11-30 13:47:31 +0530

    hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import Hero from './component/Hero/Hero';
+
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Hero/>
     </div>
   );
 }
